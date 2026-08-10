043eeb633e194b7bb42b8c7a771fbc9b147f2d3e
GARGI SINGH <160568077+gargi1p@users.noreply.github.com>
2026-08-10 16:46:02 +0530

    Delete backend directory

--- backend/utils/crypto.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-const generateRandomToken = () => crypto.randomBytes(32).toString('hex');
-
-const hashToken = (token) =>
-  crypto.createHash('sha256').update(token).digest('hex');
-
-module.exports = { generateRandomToken, hashToken };
